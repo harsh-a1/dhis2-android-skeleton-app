e6bac1f6f81d184b0f6b04d08af1e55500186d44
harsh-a1 <harsh.atal@gmail.com>
2019-07-02 15:41:38 +0530

    Intiialized D2 configuration and showed Ok message in success handler

--- app/src/main/java/com/example/android/androidskeletonapp/ui/splash/SplashActivity.java
+++ app/src/main/java/com/example/android/androidskeletonapp/ui/splash/SplashActivity.java
@@ -1,10 +1,14 @@
 package com.example.android.androidskeletonapp.ui.splash;
 
 import android.os.Bundle;
+import android.util.Log;
 
 import com.example.android.androidskeletonapp.R;
+import com.example.android.androidskeletonapp.data.Sdk;
 
 import org.hisp.dhis.android.core.D2;
+import org.hisp.dhis.android.core.d2manager.D2Configuration;
+import org.hisp.dhis.android.core.d2manager.D2Manager;
 
 import androidx.appcompat.app.AppCompatActivity;
 import io.reactivex.Single;
@@ -23,9 +27,13 @@ public class SplashActivity extends AppCompatActivity {
                 instantiateD2()
                 .doOnSuccess(d2 -> {
                     // TODO Toast success
-                    }
+
+                    Log.d("Success","Ok______________________________");
+                        }
                 ).doOnError(throwable -> {
                     // TODO Toast error
+                    Log.d("Success","Not Ok");
+
                 })
                 .subscribe();
     }
@@ -40,6 +48,12 @@ public class SplashActivity extends AppCompatActivity {
 
     private Single<D2> instantiateD2() {
         // TODO Instantiate d2
-        return Single.never();
+        final String serverURL = "http://android2.dhis2.org:8080";
+
+        Single<D2> d2Single = D2Manager.setUp(Sdk.getD2Configuration(this))
+                .andThen(D2Manager.setServerUrl(serverURL))
+                .andThen(D2Manager.instantiateD2());
+
+        return d2Single;
     }
 }
